4790fb03a9e58e4681acdb2a7b63ad73879c0620
Alexander Iansiti <aiansiti@outlook.com>
2017-05-04 21:06:34 -0400

    back at it

--- canny.py
+++ canny.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('pic.jpg', 0)
-
+img = cv2.imread('road.jpg', 0)
 
 blur = cv2.bilateralFilter(img,12,70,70)
 ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
@@ -11,7 +10,6 @@ edges = cv2.Canny(th3,1000,1000, 5)
 cv2.imshow('Original',img)
 
 lines = cv2.HoughLines(edges,1,np.pi/180,100)
-# print lines
 color = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
 for rho,theta in lines[0]:
     a = np.cos(theta)
