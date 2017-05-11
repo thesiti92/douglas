d1ff3e16c87a2a11228cd2cfdaae24b8437b9ac2
Alexander Iansiti <aiansiti@outlook.com>
2017-05-10 21:23:56 -0400

    added speed binding for interpreter

--- steering.py
+++ steering.py
@@ -2,7 +2,7 @@ from Adafruit_MotorHAT.Adafruit_PWM_Servo_Driver import PWM
 from Adafruit_MotorHAT import Adafruit_MotorHAT
 
 class steering_motor:
-    def __init__(self, controller, pim):
+    def __init__(self, controller, pin):
         self.MC = controller
         if (pin < 0) or (pin > 15):
             raise NameError('PWM pin must be between 0 and 15 inclusive')
@@ -14,7 +14,9 @@ class steering_motor:
         if (speed > 255):
             speed = 255
         self.MC._pwm.setPWM(self.PWMpin, 0, speed*16)
+mh = Adafruit_MotorHAT()
+motor = steering_motor(mh, 15)
+def setSpeed(speed):
+    motor.setSpeed(speed)
 if __name__ == "__main__":
-    mh = Adafruit_MotorHAT()
-    motor = steering_motor(mh, 15)
     motor.setSpeed(10)
