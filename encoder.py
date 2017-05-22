581c4f957bb188c5c8ce40e2ea2fe51275b3822d
Alexander Iansiti <aiansiti@outlook.com>
2017-05-22 16:49:42 -0400

    new code sample ported

--- encoder.py
+++ encoder.py
@@ -1,28 +1,57 @@
-from RPi import GPIO
-from time import sleep
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
 
-pin_24 = 24
-pin_25 = 25
+RoAPin = 24    # pin11
+RoBPin = 25    # pin12
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin_24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(pin_25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+globalCounter = 0
 
-counter = 0
-clkLastState = GPIO.input(pin_24)
+flag = 0
+Last_RoB_Status = 0
+Current_RoB_Status = 0
 
-try:
-    while True:
-        state_24 = GPIO.input(pin_24)
-        state_25 = GPIO.input(pin_25)
-        # if clkState != clkLastState:
-        #     if dtState != clkState:
-        #         counter += 1
-        #     else:
-        #         counter -= 1
-        #         print counter
-        # clkLastState = clkState
-        print(state_24, ", ", state_25)
-        sleep(0.01)
-finally:
-    GPIO.cleanup()
+def setup():
+	GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+	GPIO.setup(RoAPin, GPIO.IN)    # input mode
+	GPIO.setup(RoBPin, GPIO.IN)
+
+def rotaryDeal():
+	global flag
+	global Last_RoB_Status
+	global Current_RoB_Status
+	global globalCounter
+	Last_RoB_Status = GPIO.input(RoBPin)
+	while(not GPIO.input(RoAPin)):
+		Current_RoB_Status = GPIO.input(RoBPin)
+		flag = 1
+	if flag == 1:
+		flag = 0
+		if (Last_RoB_Status == 0) and (Current_RoB_Status == 1):
+			globalCounter = globalCounter + 1
+			print 'globalCounter = %d' % globalCounter
+		if (Last_RoB_Status == 1) and (Current_RoB_Status == 0):
+			globalCounter = globalCounter - 1
+			print 'globalCounter = %d' % globalCounter
+
+def clear(ev=None):
+        globalCounter = 0
+	print 'globalCounter = %d' % globalCounter
+	time.sleep(1)
+
+
+def loop():
+	global globalCounter
+	while True:
+		rotaryDeal()
+#		print 'globalCounter = %d' % globalCounter
+
+def destroy():
+	GPIO.cleanup()             # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
