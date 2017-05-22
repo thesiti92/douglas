035f0258cc88007f2fbab4efaac526add71b3129
Alexander Iansiti <aiansiti@outlook.com>
2017-05-22 16:39:56 -0400

    added some testing code

--- encoder.py
+++ encoder.py
@@ -1,27 +1,28 @@
 from RPi import GPIO
 from time import sleep
 
-clk = 24
-dt = 25
+pin_24 = 24
+pin_25 = 25
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(pin_24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(pin_25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 counter = 0
-clkLastState = GPIO.input(clk)
+clkLastState = GPIO.input(pin_24)
 
 try:
     while True:
-        clkState = GPIO.input(clk)
-        dtState = GPIO.input(dt)
-        if clkState != clkLastState:
-            if dtState != clkState:
-                counter += 1
-            else:
-                counter -= 1
-                print counter
-        clkLastState = clkState
+        state_24 = GPIO.input(pin_24)
+        state_25 = GPIO.input(pin_25)
+        # if clkState != clkLastState:
+        #     if dtState != clkState:
+        #         counter += 1
+        #     else:
+        #         counter -= 1
+        #         print counter
+        # clkLastState = clkState
+        print(state_24, ", ", state_25)
         sleep(0.01)
 finally:
     GPIO.cleanup()
