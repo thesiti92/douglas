2fdd8c45badb54f7b322f0fa3155427bda665ab1
Mateen Taba <mateentaba@gmail.com>
2017-05-26 12:20:13 -0400

    Updated radar code

--- radar.py
+++ radar.py
@@ -1,70 +1,41 @@
-#https://learn.adafruit.com/reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/script
-#to execute, run: chmod +x raspi-adc-pot.py
-
+#Mateen Tabatabaei
 
 import time
-import os
-import RPi.GPIO as GPIO
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
 
-GPIO.setmode(GPIO.BCM)
-DEBUG = 1
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 1
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+#channels for radars are as follows:
+#forward = 1
+#right = 2
+#left = 3
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
-
-        GPIO.output(cspin, True)
-
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
-
-# change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
-SPICLK = 18
-SPIMISO = 23
-SPIMOSI = 24
-SPICS = 25
-
-# set up the SPI interface pins
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
-
-# 10k trim pot connected to adc #0
-potentiometer_adc = 0;
-
-last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 5       # to keep from being jittery we'll only change
-                    # volume when the pot has moved more than 5 'counts'
-
+print('Reading ADS1x15 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+print('-' * 37)
 
 while True:
-        print("Object is %s mm away" %5.005*readadc(adcnum, clockpin, mosipin, misopin, cspin))
-        time.sleep(0.5)
+    # Read all the ADC channel values in a list.
+    values=[0]*4
+    for i in range(4)
+        values[i] = adc.read_adc(i, gain=GAIN)
+    
+    print("Object is %s mm away" %5.005*values[1])
+    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+    
+    time.sleep(0.5)
