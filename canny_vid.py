da01ea14208ba4e25c8c687413d061ced5e17172
eliburnes <elliot_burnes17@milton.edu>
2017-05-30 14:42:17 -0400

    average theta list, filter thetas between 0 and pi

--- canny_vid.py
+++ canny_vid.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import cv2
 import io
 import picamera
@@ -6,24 +7,44 @@ import picamera.array
 camera = picamera.PiCamera()
 stream = picamera.array.PiRGBArray(camera)
 
+theta_list=[]
+rho_list=[]
+half=np.pi/2
 
 for foo in camera.capture_continuous(stream, format='bgr'):
     ret3,frame = cv2.threshold(cv2.bilateralFilter(cv2.cvtColor(cv2.resize(stream.array, [500,500]), cv2.COLOR_BGR2GRAY),12,70,70),0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     edges = cv2.Canny(frame,1000,1000, 5)
     lines = cv2.HoughLines(edges,1,np.pi/180,100)
     print lines[0]
-#    for rho,theta in lines[0]:
-#        a = np.cos(theta)
-#        b = np.sin(theta)
-#        x0 = a*rho
-#        y0 = b*rho
-#        x1 = int(x0 + 1000*(-b))
-#        y1 = int(y0 + 1000*(a))
-#        x2 = int(x0 - 1000*(-b))
-#        y2 = int(y0 - 1000*(a))
-#
-#        cv2.line(color,(x1,y1),(x2,y2),(0,0,255),2)
-#    cv2.imshow('Trajectory',color)
+    
+    for rho,theta in lines[0]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        theta_list.append(theta)
+        rho_list.append(rho)
+        
+        cv2.line(color,(x1,y1),(x2,y2),(0,0,255),2)
+    cv2.imshow('Trajectory',color)
+
+    theta_np=np.array(theta_list)
+    rho_np = np.array(rho_list)
+
+    theta_filtered=theta_np[np.where((theta_np>=0) & (theta_np <=np.pi))]
+    rho_filtered=rho_np[np.where((rho_np>=0) & (rho_np <=np.pi))]
+
+#use list of thetas to figure out degrees to turn
+    if (np.average(theta_filtered) < half):
+        radians_to_turn = half - np.average(theta_list)
+        print "turn left", math.degrees(radians_to_turn)
+    else:
+        radians_to_turn = np.average(theta_list) - half
+        print "turn right", math.degrees(radians_to_turn)
 
     stream.truncate()
     stream.seek(0)
