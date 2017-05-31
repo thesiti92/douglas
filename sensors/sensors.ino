1c2e2fa69a01fc2b27d0569558bab42f43a751f2
Alexander Iansiti <aiansiti@outlook.com>
2017-05-31 12:12:09 -0400

    arduino sketch for aggregating sensor data

---
+++ sensors/sensors.ino
@@ -0,0 +1,21 @@
+#include <Encoder.h>
+
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(2, 3);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition*.0975);
+  }
+}
