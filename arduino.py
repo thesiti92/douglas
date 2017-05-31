02e875ceda4cbb73ec80c0b3cfd7d7da70f0a48a
JFDunn <james.dunn1798@gmail.com>
2017-05-31 14:19:21 -0400

    arduino parser stuff

--- arduino.py
+++ arduino.py
@@ -1,7 +1,23 @@
 import serial
 import sys
-ser = serial.Serial('/dev/ttyACM0', 115200)
-while True:
-	data = ser.readline()
-	if data:
-		sys.stdout.write(data.split()[0])
+import datetime
+
+def capture():
+
+	# Saves data to new file (not sure if we need this)
+	filename = "arduinoData.log"
+	text_file = open(filename, "a")
+
+	ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 0)
+
+	while True:
+		data = ser.readline()
+		if data:
+			sys.stdout.write(data.split()[0])
+
+			text_file.write(data)
+			
+	text_file.close()
+
+
+
