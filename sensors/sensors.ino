fd04b0ecacabc902ea1261e3ede0ef229d73ab6d
eliburnes <elliot_burnes17@milton.edu>
2017-06-01 15:13:37 -0400

    if statements for speed change, no hall input yet.

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -11,13 +11,26 @@ void setup() {
 }
 
 long oldPosition  = -999;
-int mph = 0;
+float old_speed = -999;
+
 
 void loop() {
   long newPosition = myEnc.read();
+  float current_speed=0; //mateen needs to get hall effect code for this to work, this should read in speed from hall sensor 
+
   if (newPosition != oldPosition) {
       oldPosition = newPosition;
       Serial.println(String(newPosition*.0975) + " " + mph);
+  }
+  if(current_speed-old_speed<-1) {
+  Serial.println(String(newPosition*.0975) + " " + mph);
+  }else {
+  	if (current_speed-oldspeed>1){
+  	Serial.println(String(newPosition*.0975) + " " + mph);
+  	}
 
   }
+  
+
+
 }
