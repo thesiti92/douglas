5dac6489dbc24eeb6f7a4d300f6dd48cb4df2f58
JFDunn <james.dunn1798@gmail.com>
2017-06-02 11:05:01 -0400

    testing the brake and throttle steppers

---
+++ stepper_motor_test.py
@@ -0,0 +1,16 @@
+import steppertest
+
+step[0] = Stepper(1)
+step[1] = Stepper(2)
+
+#Loop to test the brake and acceleration stepper motors with a manual control
+while True:
+    
+    motorChoice = raw_input("Which motor? (0 = acc, 1 = brake)")
+    motor = step[motorChoice]
+
+    steps = raw_input("How many steps forward? ")
+    motor.testDouble(steps)
+
+    steps = raw_input("How many steps backwards? ")
+    motor.testBackDouble(steps)
