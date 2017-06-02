f218f52b395871d5e18e2bd31e1910119f4e2d55
JFDunn <james.dunn1798@gmail.com>
2017-06-02 14:00:39 -0400

    even more stripped down

--- stepper_motor_test.py
+++ stepper_motor_test.py
@@ -6,6 +6,7 @@ import atexit
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT()
 
+
 # recommended for auto-disabling motors on shutdown!
 def turnOffMotors():
     mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
@@ -13,50 +14,38 @@ def turnOffMotors():
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
+    myStepper = mh.getStepper(200, 1)  # 200 steps/rev, port (1 or 2)
+    myStepper.setSpeed(40)  # 30 RPM
 
-class Stepper():
-    def __init__(self):
-        atexit.register(turnOffMotors)
-        degrees_per_step = 1.8
+def setRpm(rpm):
+    myStepper.setSpeed(rpm)  # 30 RPM
 
-        #port = raw_input("Port Number: 1 for ac, 2 for brake")
-        #print port
+def testRotate(self):
+    print("Single coil steps")
+    myStepper.step(200, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+    myStepper.step(200, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
 
-        self = mh.getStepper(200, 1)  # 200 steps/rev, port (1 or 2)
-        self.setSpeed(40)  # 30 RPM
+def testSingle(self, steps):
+    myStepper.step(self, steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
 
-    def setRpm(rpm):
-        self.setSpeed(rpm)  # 30 RPM
+def setRpm(rpm):
+    myStepper.setSpeed(rpm)
 
-    def testRotate(self):
-        print("Single coil steps")
-        self.step(200, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
-        self.step(200, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
+def testBackSingle(self, steps):
+    myStepper.step(self, steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
 
-    def testSingle(self, steps):
-        self.step(self, steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+def testBackDouble(self, steps):
+    myStepper.step(self, steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
 
-    def setRpm(rpm):
-        self.setSpeed(rpm)
+def testDouble(self, steps):
+    myStepper.step(self, steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
 
-    def testBackSingle(self, steps):
-        self.step(self, steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
-
-    def testBackDouble(self, steps):
-        self.step(self, steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
-
-    def testDouble(self, steps):
-        self.step(self, steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
-
-stepper = Stepper()
-
-#Loop to test the brake and acceleration stepper motors with a manual control
+# Loop to test the brake and acceleration stepper motors with a manual control
 while True:
+    # motor = stepper
+    steps = 10
+    # steps = raw_input("How many steps forward? ")
+    myStepper.step(steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
 
-    #motor = stepper
-
-    #steps = raw_input("How many steps forward? ")
-    #stepper.testDouble(5)
-
-    #steps = raw_input("How many steps backwards? ")
-    stepper.testBackDouble(5)
+    # steps = raw_input("How many steps backwards? ")
+    myStepper.step(steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
