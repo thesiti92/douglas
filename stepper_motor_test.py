961b84a1a295bf354a3bbf551672d781e14ca93d
JFDunn <james.dunn1798@gmail.com>
2017-06-02 12:58:38 -0400

    harrumph

--- stepper_motor_test.py
+++ stepper_motor_test.py
@@ -19,10 +19,10 @@ class Stepper():
         atexit.register(turnOffMotors)
         degrees_per_step = 1.8
 
-        port = raw_input("Port Number: 1 for ac, 2 for brake")
-        print port
+        #port = raw_input("Port Number: 1 for ac, 2 for brake")
+        #print port
 
-        self = mh.getStepper(200, port)  # 200 steps/rev, port (1 or 2)
+        self = mh.getStepper(200, 1)  # 200 steps/rev, port (1 or 2)
         self.setSpeed(40)  # 30 RPM
 
     def setRpm(rpm):
