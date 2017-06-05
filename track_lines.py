ae1831ec469fa344601bb2f0e212326d13578597
JFDunn <james.dunn1798@gmail.com>
2017-06-05 12:16:26 -0400

    added cruise control to track_lines

--- track_lines.py
+++ track_lines.py
@@ -5,6 +5,7 @@ from picamera import PiCamera
 from picamera.array import PiRGBArray
 from steering import turn, kill
 from RPi.GPIO import IN, setmode, setup, BCM, add_event_detect, BOTH, input
+from breaking import pull_brake, release_brake
 camera = PiCamera()
 stream = PiRGBArray(camera)
 brake_pin = 12
@@ -15,6 +16,10 @@ mph=0
 setmode(BCM)
 setup(brake_pin, IN)
 
+mh = Adafruit.MotorHAT()
+throttle = mh.getStepper(200, 1)  # 200 steps/rev, port (1 or 2)
+throttle.setSpeed(40)  # 40 RPM
+steps = 30
 
 def brake(channel):
     if input(brake_pin):
@@ -27,6 +32,7 @@ add_event_detect(brake_pin, BOTH, callback=brake, bouncetime=1000)
  # this callback is just to reset the steering. we shouldnt reset the cruise control because we'd need to reset the demos
 for foo in camera.capture_continuous(stream, format='bgr', resize=(640,480), use_video_port=True):
     #TODO: check mph and if off by a bit accelerate/decelerate
+
     #TODO: write clean acceleration and breaking methods to import from another file. maybe have varying degrees of acceleration or breaking.
 
     #to read in mph: need try except because arduino sometimes gives null values
@@ -34,6 +40,11 @@ for foo in camera.capture_continuous(stream, format='bgr', resize=(640,480), use
         mph = ser.readline().split()[1]
     except:
         pass
+
+    #Cruise Control: pulses throttle while speed <2 mph
+    while mph < 2 & braking == false:
+        throttle.step(self, steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
+        throttle.step(self, steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
 
     if braking:
         kill()
