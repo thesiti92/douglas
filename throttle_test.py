f286619e2fd47e2c981126346eada320ba1c7671
JFDunn <james.dunn1798@gmail.com>
2017-06-05 14:13:25 -0400

    throttle testing

--- throttle_test.py
+++ throttle_test.py
@@ -1,4 +1,5 @@
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+from stepper_motor_test import testDouble, testBackDouble
 
 import time
 import atexit
@@ -8,4 +9,5 @@ throttle = mh.getStepper(200, 1)  # 200 steps/rev, port (1 or 2)
 throttle.setSpeed(40)  # 40 RPM
 steps = 30
 
-throttle.step(throttle.step(steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE))
+testDouble(30)
+#throttle.step(throttle.step(steps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE))
