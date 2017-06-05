688771202b6974c45dfeaf4b8e041c961a235494
Alexander Iansiti <aiansiti@outlook.com>
2017-06-05 10:05:46 -0400

    Eli Has the speds

--- breaking.py
+++ breaking.py
@@ -1,9 +1,7 @@
 from Adafruit_MotorHAT import Adafruit_MotorHAT
 from RPi.GPIO import setmode, output, setup, OUT, BCM
-#from serial import Serial
-from time import time
+from time import sleep
 setmode(BCM)
-#ser = Serial('/dev/ttyACM0', 115200)
 
 
 class motor:
@@ -12,56 +10,45 @@ class motor:
         if (pwm_pin < 0) or (pwm_pin > 15):
             raise NameError('PWM pin must be between 0 and 15 inclusive')
         self.pwm_pin = pwm_pin
-        #self.sleep_pin = sleep_pin
-        #setup(sleep_pin, OUT)
         self.dir_pin = dir_pin
         setup(dir_pin, OUT)
-    
-    def pull_brake(self):
-        speed=100
-        time_on=2
-        output(self.dir_pin, 0) #decides whether brakiing or unbraking
-        self.MC._pwm.setPWM(self.pwm_pin, 0, speed)
-        start_time=time()
-        while true:
-            if time()-start_time>time_on:
-                self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
-                break
-    
-    def release_unbrake(self):
-        speed=100
-        time_on=2
-        output(self.dir_pin, 1) #decides whether brakiing or unbraking
-        self.MC._pwm.setPWM(self.pwm_pin, 0, speed)
-        start_time=time()
-        while true:
-            if time()-start_time>time_on:
-                self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
-                break
 
-    def brake(self, time):
-        speed=100
-        time_on=time
+    def pull_brake(self, speed=100, time_on=1):
         output(self.dir_pin, 0) #decides whether brakiing or unbraking
         self.MC._pwm.setPWM(self.pwm_pin, 0, speed)
-        start_time=time()
-        while true:
-            if time()-start_time>time_on:
-                self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
-                break # could immediately switch directions and go backwards in this while loop...
+        sleep(time_on)
+        self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
+
+    def release_brake(self, speed=100, time_on=1):
         output(self.dir_pin, 1) #decides whether brakiing or unbraking
         self.MC._pwm.setPWM(self.pwm_pin, 0, speed)
-        start_time=time()
-        while true:
-            if time()-start_time>time_on:
-                self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
-                break
+        sleep(time_on)
+        self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
+
+###ELI HAS THE SPEDS
+    # def brake(self, time):
+    #     speed=100
+    #     time_on=time
+    #     output(self.dir_pin, 0) #decides whether brakiing or unbraking
+    #     self.MC._pwm.setPWM(self.pwm_pin, 0, speed)
+    #     start_time=time()
+    #     while true:
+    #         if time()-start_time>time_on:
+    #             self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
+    #             break # could immediately switch directions and go backwards in this while loop...
+    #     output(self.dir_pin, 1) #decides whether brakiing or unbraking
+    #     self.MC._pwm.setPWM(self.pwm_pin, 0, speed)
+    #     start_time=time()
+    #     while true:
+    #         if time()-start_time>time_on:
+    #             self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
+    #             break
 
     def kill(self):
-        self.setSpeed(0)
+        self.MC._pwm.setPWM(self.pwm_pin, 0, 0)
 
 mh = Adafruit_MotorHAT()
-brake_motor = motor(mh, 15, 23) #create break motor. pwm pin 14,
-
-#testing the motor
-brake_motor.brake(.5)
+brake_motor = motor(mh, 14, 23) #create break motor. pwm pin 14,
+if __name__ == "__main__":
+    #testing the motor
+    brake_motor.brake(.5)
