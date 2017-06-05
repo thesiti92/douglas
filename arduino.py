d0c93bb3a0588779388c0806d71521f28b8d44bd
Alexander Iansiti <aiansiti@outlook.com>
2017-06-05 11:37:04 -0400

    stripped it down

--- arduino.py
+++ arduino.py
@@ -2,19 +2,9 @@ import serial
 import sys
 import datetime
 
-def captureData():
 
-	# Saves data to new file (not sure if we need this)
-	ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 0)
+# Saves data to new file (not sure if we need this)
+ser = serial.Serial('/dev/ttyACM1', 115200, timeout = 0)
 
-	while True:
-		data = ser.readline()
-		if data:
-			sys.stdout.write(data.split())
-			encoder = data.split()[0]
-			speed = data.split()[1]
-			return encoder, speed
-
-
-
-
+while True:
+	print ser.readline()
