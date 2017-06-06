270ce041c5f9e31c37c06a90a9ad26c53f022c36
JFDunn <james.dunn1798@gmail.com>
2017-06-06 15:21:19 -0400

    added try catch

--- track_lines.py
+++ track_lines.py
@@ -45,45 +45,51 @@ def brake(channel):
         braking = False
 add_event_detect(brake_pin, BOTH, callback=brake, bouncetime=1000)
  # this callback is just to reset the steering. we shouldnt reset the cruise control because we'd need to reset the demos
-for foo in camera.capture_continuous(stream, format='bgr', resize=(640,480), use_video_port=True):
-    #TODO: check mph and if off by a bit accelerate/decelerate
+def loop():
+    for foo in camera.capture_continuous(stream, format='bgr', resize=(640,480), use_video_port=True):
+        #TODO: check mph and if off by a bit accelerate/decelerate
 
-    #TODO: write clean acceleration and breaking methods to import from another file. maybe have varying degrees of acceleration or breaking.
-    print braking
-    #to read in mph: need try except because arduino sometimes gives null values
+        #TODO: write clean acceleration and breaking methods to import from another file. maybe have varying degrees of acceleration or breaking.
+        print braking
+        #to read in mph: need try except because arduino sometimes gives null values
 
-    # try:
-    #     if ser.readline().split()[1] < 2 & braking == False:
-    #         thr.start()
-    # except:
-    #     pass
+        # try:
+        #     if ser.readline().split()[1] < 2 & braking == False:
+        #         thr.start()
+        # except:
+        #     pass
 
-    if braking:
-        if braked == False:
-            dobrake(speed=1000)
-            braked == True
-        kill()
-        print "we braked"
-        #TODO: disengage accelerator motor
-        continue
-    ret3,frame = threshold(bilateralFilter(cvtColor(stream.array, COLOR_BGR2GRAY),12,70,70),0,255,THRESH_BINARY+THRESH_OTSU)
-    edges = Canny(frame,1000,1000, 5)
-    lines = HoughLines(edges,1,pi/180,100)
-    if lines is None:
-        kill()
+        if braking:
+            if braked == False:
+                dobrake(speed=1000)
+                braked == True
+            kill()
+            print "we braked"
+            #TODO: disengage accelerator motor
+            continue
+        ret3,frame = threshold(bilateralFilter(cvtColor(stream.array, COLOR_BGR2GRAY),12,70,70),0,255,THRESH_BINARY+THRESH_OTSU)
+        edges = Canny(frame,1000,1000, 5)
+        lines = HoughLines(edges,1,pi/180,100)
+        if lines is None:
+            kill()
+            stream.truncate()
+            stream.seek(0)
+            continue
+        thetas=array([theta for rho, theta in lines[0]])
+        theta_filtered=thetas[where((thetas>=0) & (thetas <=pi))]
+
+        #use list of thetas to figure out degrees to turn
+        if (average(theta_filtered) < half):
+            radians_to_turn = half - average(thetas)
+            turn(degrees(radians_to_turn), dir=False)
+        else:
+            radians_to_turn = average(thetas) - half
+            turn(degrees(radians_to_turn), dir=True)
         stream.truncate()
         stream.seek(0)
-        continue
-    thetas=array([theta for rho, theta in lines[0]])
-    theta_filtered=thetas[where((thetas>=0) & (thetas <=pi))]
 
-    #use list of thetas to figure out degrees to turn
-    if (average(theta_filtered) < half):
-        radians_to_turn = half - average(thetas)
-        turn(degrees(radians_to_turn), dir=False)
-    else:
-        radians_to_turn = average(thetas) - half
-        turn(degrees(radians_to_turn), dir=True)
-    stream.truncate()
-    stream.seek(0)
-
+if __name__ == "__name__": 
+    try:
+        loop()
+    except:
+        loop()
